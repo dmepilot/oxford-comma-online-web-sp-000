53a35196b8b6cf4f84ce50664ad20dc0b5ca4582
Dean Erickson <dmepilot@yahoo.com>
2020-06-06 06:25:36 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,7 +4,7 @@ def oxford_comma(array)
   elsif array.size == 2 
     array.join(" and ")
   elsif array.size > 2
-  array.join(", ")
-  array.insert(/.*\G,\s/, "and ")
+  string=array.join(", ")
+  string.insert(/.*\G,\s/, "and ")
 end
 end
